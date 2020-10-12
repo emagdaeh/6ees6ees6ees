4939009e41ce9f242a163ce71936850de5176031
Adam Whitman <adam.whitmanpt@gmail.com>
2020-10-12 13:22:23 -0700

    Finished ES6 Bees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends HoneyMakerBee {
+  constructor (age, job, canFly, treasureChest) {
+    super(ForagerBee);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,19 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor (age, job, canFly, color) {
+    super(RetiredForagerBee);
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+  }
+
+  forage(no) {
+    no = 'I am too old, let me play cards instead';
+    return no;
+  }
+
+  gamble(treasure) {
+    return this.treasureChest.push(treasure);
+  }
+
 };
